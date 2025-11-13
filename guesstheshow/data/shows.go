137489c5f3ea6a455845c0fa1f4bfcba4c72fbde
PBJswich <p.biteng11@gmail.com>
2025-11-12 21:30:22 -0600

    added time per turn and some more shows

--- guesstheshow/data/shows.go
+++ guesstheshow/data/shows.go
@@ -2,8 +2,8 @@ package data
 
 // Shows is a map of genre → list of words
 var Shows = map[string][]string{
-	"Drama":           {"landman", "the rookie", "fallout", "breaking bad", "game of thrones"},
-	"Comedy":          {"smiling friends", "the office", "south park", "community", "friends"},
-	"Horror":          {"the last of us", "dahmer", "stranger things", "haunted", "american horror story"},
-	"Science-Fiction": {"the boys", "futurama", "the flash", "solar opposites", "rick and morty"},
+	"Drama":           {"landman", "the rookie", "fallout", "breaking bad", "game of thrones", "yellowstone", "the wire", "the sopranos", "better call saul"},
+	"Comedy":          {"smiling friends", "the office", "south park", "community", "friends", "curb your enthusiasm", "veep", "arrested development", "parks and recreation"},
+	"Horror":          {"the last of us", "dahmer", "stranger things", "haunted", "american horror story", "the walking dead", "black mirror", "midnight mass"},
+	"Science-Fiction": {"the boys", "futurama", "the flash", "solar opposites", "rick and morty", "severance", "doctor who", "the mandalorian", "altered carbon"},
 }
